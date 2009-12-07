81a55a6ff3ac6492045c820ad20a3e2edf2482d9
Peter Boström <lemming@phobos.(none)>
2009-12-07 04:29:38 +0100

    halvfärdig internetlab

--- progp09/I/client.c
+++ progp09/I/client.c
@@ -5,23 +5,30 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <unistd.h>
 
 // fixed-size types
 #include <stdint.h>
 
 struct
 {
-	char welcome_msg[80+1]; // 80 + \0
-	char username_msg[16+1];
-	char pin_msg[16+1];
-	char shortcode_msg[16+1];
+	char user[16+1];
+	char pin[16+1];
+	char shortcode[16+1];
+	char balance[16+1];
+	char withdraw[16+1];
+	char exit[16+1];
 } lang;
+
+char welcome_msg[80+1]; // 80 + \0
+
+char buffer[128];
 
 int main(int argc, char *argv[])
 {
 	char *address, *port;
 
-	printf("BANKINATOR BOOTUP SEQUENCE!\n\n\n");
+	printf("BANKINATOR BOOTUP SEQUENCE!\n");
 
 	if(argc > 1)
 		address = argv[1];
@@ -56,33 +63,94 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
+	// select language
+
+	strcpy(welcome_msg, "wellcome yo");
+	strcpy(lang.user, "card number");
+	strcpy(lang.pin, "pin code");
+	strcpy(lang.shortcode, "single-use code");
+	strcpy(lang.balance, "balance");
+	strcpy(lang.withdraw, "withdraw");
+	strcpy(lang.exit, "exit");
+
 	if(connect(c_socket, addr->ai_addr, addr->ai_addrlen) != 0)
 	{
 		perror("connect");
 		return -1;
 	}
 
-	// select language
-
-	strcpy(lang.welcome_msg, "wellcome yo");
-	strcpy(lang.username_msg, "username");
-	strcpy(lang.pin_msg, "pin code");
-	strcpy(lang.shortcode_msg, "single-use code");
-
 	while(1)
 	{
-		printf("%s\n", lang.welcome_msg);
+		putchar('\n');
 
-		printf("%s: ", lang.username_msg);
-		uint32_t username;
-		scanf("%u", &username);
+		*buffer = 'm';
+		send(c_socket, buffer, 1, 0);
+		recv(c_socket, welcome_msg, 80+1, 0);
+
+		printf("%s\n", welcome_msg);
+
+		printf("%s: ", lang.user);
+		uint32_t user;
+		scanf("%u", &user);
 
 		uint16_t pin;
-		printf("%s: ", lang.pin_msg);
+		printf("%s: ", lang.pin);
 		scanf("%hu", &pin);
+
+		*buffer = 'l';
+		*(uint32_t *)(buffer+1) = htonl(user);
+		*(uint16_t *)(buffer+5) = htons(pin);
+
+		send(c_socket, buffer, 7, 0);
+		if(recv(c_socket, buffer, 1, MSG_WAITALL) == -1)
+		{
+			printf("fail :(\n");
+			perror("recv");
+			return -1;
+		}
+		if(*buffer != 'l')
+		{
+			printf("login failed\n");
+			continue;
+		}
+		while(1)
+		{
+			printf("(b) %s (w) %s (e) %s\n", lang.balance, lang.withdraw, lang.exit);
+			scanf("%s", buffer);
+			if(buffer[1] != '\0')
+				continue;
+			if(*buffer == 'e')
+				break;
+
+			switch(*buffer)
+			{
+				case 'b':
+					send(c_socket, buffer, 1, 0);
+					recv(c_socket, buffer, 5, 0);
+					if(*buffer != 'b')
+					{
+						printf("protocol error, unable to retrieve balance.\n");
+						return -1;
+					}
+					printf("%s: %f\n", lang.balance, *(float *)(buffer+1));
+					break;
+				case 'w':
+					printf("no, I like hot tamale\n");
+					break;
+				default:
+					break;
+			}
+		}
 	}
 
 	freeaddrinfo(addr);
+
+	if(close(c_socket) == -1)
+	{
+		perror("close");
+		return -1;
+	}
+
 
 	return 0;
 }
--- progp09/I/server.c
+++ progp09/I/server.c
@@ -5,15 +5,42 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <fcntl.h>
+
+#include <stdint.h>
 
 int s_socket;
 
-#define conn_count 42
-int conn[conn_count];
-int next_conn = 0;
+char welcome_msg[80+1] = "hax0r bank connected.";
+
+typedef struct
+{
+	uint32_t user;
+	uint16_t pin;
+
+	float money;
+} user;
+
+user users[3] = {{12456789, 4711, 42},
+		{101010, 10, 101.0},
+		{1524324, 2034, 10234.0}};
+
+int user_len = 3;
+
+struct
+{
+	int socket;
+
+	user *account;
+} conn[42];
+int connections = 0;
+
+char buffer[10];
 
 int main(int argc, char *argv[])
 {
+	int i;
+
 	struct sockaddr_in6 addr;
 	memset(&addr, 0, sizeof(addr));
 
@@ -24,6 +51,9 @@ int main(int argc, char *argv[])
 	socklen_t addrlen = sizeof(addr);
 
 	s_socket = socket(addr.sin6_family, SOCK_STREAM, 0);
+
+	fcntl(0, F_SETFL, O_NONBLOCK);
+	fcntl(s_socket, F_SETFL, O_NONBLOCK);
 
 	if(s_socket == -1)
 	{
@@ -43,22 +73,102 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	int foo = accept(s_socket, (struct sockaddr *) &addr, &addrlen);
-
-	if(foo == -1)
+	while(1)
 	{
-		perror("accept");
-		return -1;
-	}
+		int foo = accept(s_socket, (struct sockaddr *) &addr, &addrlen);
 
-	send(foo, "hello! :)", sizeof("hello! :)"), 0);
+		if(foo != -1)
+		{
+			printf("connected!\n");
 
-	printf("someone connected, that's kickass!\n");
+			fcntl(foo, F_SETFL, O_NONBLOCK);
 
-	if(close(foo) == -1)
-	{
-		perror("close socket");
-		return -1;
+			conn[connections].socket = foo;
+			conn[connections].account = NULL;
+			connections++;
+		}
+
+		for(i = 0; i < connections; ++i)
+		{
+			int retval = recv(conn[i].socket, buffer, 1, 0);
+			if(retval == 0)
+			{
+				close(conn[i].socket);
+				conn[i--] = conn[--connections];
+				continue;
+			}
+			else if(retval != -1)
+			{
+				uint32_t user;
+				uint16_t pin;
+				int j;
+				float money;
+				uint8_t code;
+				switch(*buffer)
+				{
+					// login, sets account
+					case 'l':
+						recv(conn[i].socket, &user, sizeof(uint32_t), 0);
+						user = ntohl(user);
+						recv(conn[i].socket, &pin, sizeof(uint16_t), 0);
+						pin = ntohs(pin);
+						printf("login: %d:%d\n", user, pin);
+						*buffer = 'e';
+						// checks for user matching that ID
+						for(j = 0; j < user_len; ++j)
+						{
+							if(users[j].user == user)
+							{
+								if(users[j].pin == pin)
+								{
+									conn[i].account = &users[j];
+									*buffer = 'l';
+								}
+								break;
+							}
+						}
+						send(conn[i].socket, buffer, 1, 0);
+						break;
+					// welcome message
+					case 'm':
+						send(conn[i].socket, welcome_msg, strlen(welcome_msg)+1, 0);
+						break;
+					// balance
+					case 'b':
+						if(conn[i].account == NULL)
+						{
+							*buffer = 'e';
+							send(conn[i].socket, buffer, 1, 0);
+							break;
+						}
+						*(float *)(buffer+1) = conn[i].account->money;
+						send(conn[i].socket, buffer, 5, 0);
+						break;
+					// withdraw (subtract) money
+					case 'w':
+						recv(conn[i].socket, &code, 1, 0);
+						recv(conn[i].socket, &money, sizeof(float), 0);
+
+						if(conn[i].account == NULL || conn[i].account->money < money)
+						{
+							*buffer = 'e';
+							send(conn[i].socket, buffer, 1, 0);
+							break;
+						}
+
+						*buffer = 'w';
+						*(float *)(buffer+1) = money;
+
+						send(conn[i].socket, buffer, 5, 0);
+						break;
+					default:
+						putchar(*buffer);
+						printf("errorah! :(\n");
+				}
+			}
+		}
+
+		fgets(welcome_msg, 80+1, stdin);
 	}
 
 	if(close(s_socket) == -1)
@@ -66,6 +176,8 @@ int main(int argc, char *argv[])
 		perror("close");
 		return -1;
 	}
+
+	//printf("close sockets.. :)");
 
 	return 0;
 }
