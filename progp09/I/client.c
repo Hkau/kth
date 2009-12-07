8f69c5c10d6e768c0d90fd07e1ae5d4fb40c3e89
Peter Boström <lemming@phobos.(none)>
2009-12-07 06:17:42 +0100

    klart :)

--- progp09/I/client.c
+++ progp09/I/client.c
@@ -10,8 +10,11 @@
 // fixed-size types
 #include <stdint.h>
 
+// current language strings
 struct
 {
+	char name[2];
+
 	char user[16+1];
 	char pin[16+1];
 	char shortcode[16+1];
@@ -20,8 +23,10 @@ struct
 	char exit[16+1];
 } lang;
 
+// 80-char welcome message as specified
 char welcome_msg[80+1]; // 80 + \0
 
+// buffer used for sending/recieving data
 char buffer[128];
 
 int main(int argc, char *argv[])
@@ -30,6 +35,7 @@ int main(int argc, char *argv[])
 
 	printf("BANKINATOR BOOTUP SEQUENCE!\n");
 
+	// read address/port arguments if existing
 	if(argc > 1)
 		address = argv[1];
 	else
@@ -40,21 +46,26 @@ int main(int argc, char *argv[])
 	else
 		port = "4711";
 
+	// addresses, for converting string to address
 	struct addrinfo *addr, hints;
 
 	memset(&hints, 0, sizeof(hints));
 
+	// may be ipv6 or ipv4, both work
 	hints.ai_family = PF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 
+	// resolve address
 	int err = getaddrinfo(address, port, &hints, &addr);
 
+	// print error and quit if one occured
 	if(err != 0)
 	{
 		fprintf(stderr, "lookup: %s", gai_strerror(err));
 		return err;
 	}
 
+	// create client socket
 	int c_socket = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
 
 	if(c_socket == -1)
@@ -63,33 +74,88 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	// select language
-
-	strcpy(welcome_msg, "wellcome yo");
-	strcpy(lang.user, "card number");
-	strcpy(lang.pin, "pin code");
-	strcpy(lang.shortcode, "single-use code");
-	strcpy(lang.balance, "balance");
-	strcpy(lang.withdraw, "withdraw");
-	strcpy(lang.exit, "exit");
-
+	// connect to server
 	if(connect(c_socket, addr->ai_addr, addr->ai_addrlen) != 0)
 	{
 		perror("connect");
 		return -1;
 	}
 
+	// set language name to something invalid so that it'll update
+	lang.name[0] = 0;
+
 	while(1)
 	{
 		putchar('\n');
 
+		// request welcome message
 		*buffer = 'm';
 		send(c_socket, buffer, 1, 0);
 		recv(c_socket, welcome_msg, 80+1, 0);
 
 		printf("%s\n", welcome_msg);
 
-		printf("%s: ", lang.user);
+		// recieve and print language list
+		printf("LANGUAGE: ");
+		*buffer = '?';
+		send(c_socket, buffer, 1, 0);
+		recv(c_socket, buffer, sizeof(buffer), 0);
+
+		// print out all recieved language codes
+		int i;
+		for(i = 0; i < buffer[1]; ++i)
+		{
+			putchar('\n');
+			putchar(buffer[2+2*i]);
+			putchar(buffer[2+2*i+1]);
+		}
+
+		// select language
+		printf("\nchoose language: ");
+
+		char langname[4];
+		scanf("%s", langname);
+
+		if(langname[2] != '\0')
+			continue;
+
+		// download if language isn't the current one
+		if(lang.name[0] != langname[0] || lang.name[1] != langname[1])
+		{
+			// send language request
+
+			*buffer = 'x';
+			buffer[1] = langname[0];
+			buffer[2] = langname[1];
+			send(c_socket, buffer, 3, 0);
+
+			// check error/message code
+			recv(c_socket, buffer, 1, 0);
+			if(*buffer != 'x')
+				continue;
+
+			// OK! read rest of message
+			recv(c_socket, buffer, sizeof(buffer), 0);
+			lang.name[0] = langname[0];
+			lang.name[1] = langname[1];
+
+			// ugly thing copies back from buffer to languages each 16+'\0' string
+			int k = 0;
+			memcpy(lang.user, buffer+k, 16+1);
+			k += 16+1;
+			memcpy(lang.pin, buffer+k, 16+1);
+			k += 16+1;
+			memcpy(lang.shortcode, buffer+k, 16+1);
+			k += 16+1;
+			memcpy(lang.balance, buffer+k, 16+1);
+			k += 16+1;
+			memcpy(lang.withdraw, buffer+k, 16+1);
+			k += 16+1;
+			memcpy(lang.exit, buffer+k, 16+1);
+		}
+
+		// login sequence
+		printf("\n%s: ", lang.user);
 		uint32_t user;
 		scanf("%u", &user);
 
@@ -97,52 +163,84 @@ int main(int argc, char *argv[])
 		printf("%s: ", lang.pin);
 		scanf("%hu", &pin);
 
+		// htonl/ntohl is important for converting to network endianism
 		*buffer = 'l';
 		*(uint32_t *)(buffer+1) = htonl(user);
 		*(uint16_t *)(buffer+5) = htons(pin);
 
+		// send login request
 		send(c_socket, buffer, 7, 0);
-		if(recv(c_socket, buffer, 1, MSG_WAITALL) == -1)
+		if(recv(c_socket, buffer, 1, 0) == -1)
 		{
 			printf("fail :(\n");
 			perror("recv");
 			return -1;
 		}
+		// restart if login failed
 		if(*buffer != 'l')
 		{
 			printf("login failed\n");
 			continue;
 		}
+		// login ok, proceed to check out the account
 		while(1)
 		{
+			// menu, awesome thing stuff
 			printf("(b) %s (w) %s (e) %s\n", lang.balance, lang.withdraw, lang.exit);
 			scanf("%s", buffer);
+
 			if(buffer[1] != '\0')
 				continue;
 			if(*buffer == 'e')
 				break;
 
+			float money;
+			short code;
+
+			// check menu choice (first character)
 			switch(*buffer)
 			{
+				// check account balance
 				case 'b':
 					send(c_socket, buffer, 1, 0);
 					recv(c_socket, buffer, 5, 0);
 					if(*buffer != 'b')
 					{
-						printf("protocol error, unable to retrieve balance.\n");
+						printf("balance error\n");
 						return -1;
 					}
 					printf("%s: %f\n", lang.balance, *(float *)(buffer+1));
 					break;
+				// withdraw money from account
 				case 'w':
-					printf("no, I like hot tamale\n");
+					// ask for short single-use code
+					printf("%s: ", lang.shortcode);
+					scanf("%hu", &code);
+
+					// ask for amount to withdraw
+					printf("%s: ", lang.withdraw);
+					scanf("%f", &money);
+
+					// strip single-use code down to 1 byte
+					*(uint8_t *)(buffer+1) = code & 0xFF;
+					*(float *)(buffer+2) = money;
+
+					// send withdraw request
+					send(c_socket, buffer, 6, 0);
+					recv(c_socket, buffer, 5, 0);
+
+					// say that an error occured if withdraw request didn't fall through
+					if(*buffer != 'w')
+						printf("error");
 					break;
+				// invalid option. :)
 				default:
 					break;
 			}
 		}
 	}
 
+	// good stuff to do, unfortunately the client won't reach this point, foreverloopstyle.
 	freeaddrinfo(addr);
 
 	if(close(c_socket) == -1)
--- progp09/I/server.c
+++ progp09/I/server.c
@@ -9,10 +9,13 @@
 
 #include <stdint.h>
 
+// server listening socket
 int s_socket;
 
+// welcome message with default message, can be changed later
 char welcome_msg[80+1] = "hax0r bank connected.";
 
+// user representation
 typedef struct
 {
 	uint32_t user;
@@ -21,52 +24,85 @@ typedef struct
 	float money;
 } user;
 
+// the bank's users
 user users[3] = {{12456789, 4711, 42},
 		{101010, 10, 101.0},
 		{1524324, 2034, 10234.0}};
 
+// number of users (length of list)
 int user_len = 3;
 
+// connections
 struct
 {
 	int socket;
 
 	user *account;
 } conn[42];
+
+// existing connections
 int connections = 0;
 
+// buffer used for sending packets
 char buffer[10];
+
+// languages contain these strings (and a name)
+typedef struct
+{
+	char name[2];
+
+	char user[16+1];
+	char pin[16+1];
+	char shortcode[16+1];
+	char balance[16+1];
+	char withdraw[16+1];
+	char exit[16+1];
+} lang;
+
+// languages, english and swedish
+lang langs[2] = {{{'e','n'}, "card number", "pin code", "single-use code", "balance", "withdraw", "exit"},
+		{{'s','v'}, "kortnummer", "pin-kod", "engångskod", "saldo", "utdrag", "avsluta"}};
+
+// number of languages
+char lang_len = 2;
 
 int main(int argc, char *argv[])
 {
 	int i;
 
+	// create a socket address
 	struct sockaddr_in6 addr;
 	memset(&addr, 0, sizeof(addr));
 
+	// ipv6 socket, localhost, 4711
 	addr.sin6_family = AF_INET6;
 	addr.sin6_port = htons(4711);
 	addr.sin6_addr = in6addr_any;
 
 	socklen_t addrlen = sizeof(addr);
 
+	// create socket
 	s_socket = socket(addr.sin6_family, SOCK_STREAM, 0);
 
-	fcntl(0, F_SETFL, O_NONBLOCK);
-	fcntl(s_socket, F_SETFL, O_NONBLOCK);
-
+	// if socket couldn't be made, print error
 	if(s_socket == -1)
 	{
 		perror("socket");
 		return -1;
 	}
 
+	// set socket and stdin to be non-blocking, so that we can handle multiple inputs
+	fcntl(0, F_SETFL, O_NONBLOCK);
+	fcntl(s_socket, F_SETFL, O_NONBLOCK);
+
+	// bind socket to address
 	if(bind(s_socket, (struct sockaddr *) &addr, addrlen) == -1)
 	{
 		perror("bind");
 		return -1;
 	}
 
+	// start listening for connections (42 max)
 	if(listen(s_socket, 42) == -1)
 	{
 		perror("listen");
@@ -75,43 +111,97 @@ int main(int argc, char *argv[])
 
 	while(1)
 	{
+		// try accepting at socket
 		int foo = accept(s_socket, (struct sockaddr *) &addr, &addrlen);
 
+		// if someone new connected
 		if(foo != -1)
 		{
 			printf("connected!\n");
 
+			// set this new socket to be non-blocking
 			fcntl(foo, F_SETFL, O_NONBLOCK);
 
+			// create a connection struct for this client
 			conn[connections].socket = foo;
 			conn[connections].account = NULL;
 			connections++;
 		}
 
+		// handle all connections' 
 		for(i = 0; i < connections; ++i)
 		{
 			int retval = recv(conn[i].socket, buffer, 1, 0);
+			// client exited
 			if(retval == 0)
 			{
+
 				close(conn[i].socket);
 				conn[i--] = conn[--connections];
 				continue;
 			}
+			// client has sent a message
 			else if(retval != -1)
 			{
 				uint32_t user;
 				uint16_t pin;
-				int j;
+				int j, k;
 				float money;
 				uint8_t code;
+				char langname[2];
+
+				// what kind of message?
 				switch(*buffer)
 				{
+					// language listing
+					case '?':
+						buffer[1] = lang_len;
+						for(j = 0; j < lang_len; ++j)
+						{
+							// write language names into buffer
+							buffer[2+2*j] = langs[j].name[0];
+							buffer[2+2*j+1] = langs[j].name[1];
+						}
+						send(conn[i].socket, buffer, 2+2*lang_len, 0);
+						break;
+					// send language
+					case 'x':
+						recv(conn[i].socket, &langname, 2, 0);
+						*buffer = 'e';
+						for(j = 0; j < lang_len; ++j)
+						{
+							// if the language searched for exists
+							if(langs[j].name[0] != langname[0] || langs[j].name[1] != langname[1])
+								continue;
+							// we have a valid language (reply isn't 'e' anymore)
+							*buffer = 'x';
+							// aaand copy all strings into the buffer
+							k = 1;
+							memcpy(buffer+k, langs[j].user, 16+1);
+							k+=16+1;
+							memcpy(buffer+k, langs[j].pin, 16+1);
+							k+=16+1;
+							memcpy(buffer+k, langs[j].shortcode, 16+1);
+							k+=16+1;
+							memcpy(buffer+k, langs[j].balance, 16+1);
+							k+=16+1;
+							memcpy(buffer+k, langs[j].withdraw, 16+1);
+							k+=16+1;
+							memcpy(buffer+k, langs[j].exit, 16+1);
+							k+=16+1;
+						}
+						// send reply (error or all strings)
+						send(conn[i].socket, buffer, k, 0);
+						break;
 					// login, sets account
 					case 'l':
+						// read user name
 						recv(conn[i].socket, &user, sizeof(uint32_t), 0);
 						user = ntohl(user);
+						// read user pass
 						recv(conn[i].socket, &pin, sizeof(uint16_t), 0);
 						pin = ntohs(pin);
+						// log login in plaintext, for that security extravaganza
 						printf("login: %d:%d\n", user, pin);
 						*buffer = 'e';
 						// checks for user matching that ID
@@ -121,56 +211,68 @@ int main(int argc, char *argv[])
 							{
 								if(users[j].pin == pin)
 								{
+									// valid login, associate connection with account
 									conn[i].account = &users[j];
 									*buffer = 'l';
 								}
 								break;
 							}
 						}
+						// send reply (error or login (e/l))
 						send(conn[i].socket, buffer, 1, 0);
 						break;
 					// welcome message
 					case 'm':
+						// simply send the stored welcome message
 						send(conn[i].socket, welcome_msg, strlen(welcome_msg)+1, 0);
 						break;
 					// balance
 					case 'b':
+						// make sure that the account is logged on
 						if(conn[i].account == NULL)
 						{
 							*buffer = 'e';
 							send(conn[i].socket, buffer, 1, 0);
 							break;
 						}
+						// send back status
 						*(float *)(buffer+1) = conn[i].account->money;
 						send(conn[i].socket, buffer, 5, 0);
 						break;
 					// withdraw (subtract) money
 					case 'w':
+						// recieve single-digit code
 						recv(conn[i].socket, &code, 1, 0);
+						// money to be withdrawn
 						recv(conn[i].socket, &money, sizeof(float), 0);
 
-						if(conn[i].account == NULL || conn[i].account->money < money)
+						// invalid if user isn't logged in, doesn't have enough money, or single-use code is invalid
+						if(conn[i].account == NULL || conn[i].account->money < money || code % 2 != 1)
 						{
 							*buffer = 'e';
 							send(conn[i].socket, buffer, 1, 0);
 							break;
 						}
 
-						*buffer = 'w';
+						// send back reply confirming removed amount just to be sure.
 						*(float *)(buffer+1) = money;
 
+						// send reply and remove moneys
 						send(conn[i].socket, buffer, 5, 0);
+						conn[i].account->money -= money;
 						break;
 					default:
-						putchar(*buffer);
+						// invalid packet
 						printf("errorah! :(\n");
 				}
 			}
 		}
 
+		// update welcome message if one has been entered, this doesn't block and won't replace unless one has been entered
 		fgets(welcome_msg, 80+1, stdin);
 	}
 
+	// this would normally close the server socket, but the server is stuck in a foreverloop.
 	if(close(s_socket) == -1)
 	{
 		perror("close");
