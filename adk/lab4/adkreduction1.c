d69f3402c360aa2d58b2f263239e5d5ee1367cae
Peter Boström <lemming@phobos.(none)>
2010-04-26 13:16:13 +0200

    orka + adk, lab4start

---
+++ adk/lab4/adkreduction1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static inline int readint()
+{
+	register int num = 0;
+
+	register char c = getchar();
+
+	while(c < '0' || c > '9')
+		c = getchar();
+
+	do
+	{
+		num = num*10 + c - '0';
+		c = getchar();
+	} while(c >= '0' && c <= '9');
+
+	return num;
+}
+
+int main()
+{
+	return 0;
+}
