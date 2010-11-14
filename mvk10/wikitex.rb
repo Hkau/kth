7fe8eab61d101468ef0bf8af232c9fd3fc65a5c5
Peter Boström <lemming@phobos.(none)>
2010-11-14 21:51:20 +0100

    ändrat till docclass report samt moddat scriptet för att fixa chapters

--- mvk10/wikitex.rb
+++ mvk10/wikitex.rb
@@ -2,12 +2,15 @@ puts open('nyx-head.tex').read
 
 input = $stdin.read
 
-split = input.split(/(^h[123]\. .*)/)
+split = input.split(/(^h[1-4]\. .*)/)
 
 # Get rid of stray spaces and newline characters.
 split.each do |section|
   section.strip!
   # replace *format* _format_ and +format+
+  section.gsub!(/^(\*+)\s/) {|match| '>> ' * (match.size-1) }
+  section.gsub!(/\"(.+?)\"/,  '``\1\'\'')
+  section.gsub!(/\[http:\/\/(.+?)\]/, '\url{http://\1}')
   section.gsub!(/\+(.+?)\+/, '\underline{\1}')
   section.gsub!(/_(.+?)_/, '\emph{\1}')
   section.gsub!(/\*(.+?)\*/, '\textbf{\1}')
@@ -43,8 +46,17 @@ end
 # eat the rest, nom nom :3
 while not split.empty?
   #puts split[0]
-  section = split[0].match /^h([123])\. \s*(.*)\s*$/
-  print "\t" * (section[1].to_i - 1), '\\', 'sub'*(section[1].to_i - 1), 'section{', section[2], '}'
+  section = split[0].match /^h([1-4])\.\s+(.*)\s*$/
+  if section[2] == "appendix"
+    puts "\t\\appendix"
+    split.shift
+    split.shift
+    next
+  elsif section[1] == '1'
+    print '\chapter{',section[2],'}'
+  else
+    print "\t" * (section[1].to_i - 2), '\\', 'sub'*(section[1].to_i - 2), 'section{', section[2], '}'
+  end
   puts
   split[1].each do |line|
     print "\t" * (section[1].to_i), line
@@ -70,3 +82,4 @@ split.each do |par|
 end
 
 puts open('nyx-foot.tex').read
+
