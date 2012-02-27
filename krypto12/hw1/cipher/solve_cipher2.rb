cc9b5c55939c71f2354e9b16afef311639c3197c
Peter Boström <pbos@kth.se>
2012-02-27 13:27:53 +0100

    hw1 structure

---
+++ krypto12/hw1/cipher/solve_cipher2.rb
@@ -0,0 +1,77 @@
+#!/usr/bin/env ruby
+translation = {
+	'' => ' ', 
+	'' => "\n", 
+	'' => 'e', 
+	'' => 't', 
+	'' => 'a', 
+	'' => 'o', 
+	'' => 'i', 
+	'' => 'n', 
+	'' => 's', 
+	'' => 'h', 
+	'' => 'r', 
+	'' => 'd', 
+	'' => 'l', 
+	'' => 'c', 
+	'' => 'u', 
+	'' => 'm', 
+	'' => 'w', 
+	'' => 'f', 
+	'' => 'g', 
+	'' => 'y', 
+	'' => 'p', 
+	'' => 'b', 
+	'' => 'v', 
+	'' => 'k', 
+	'' => 'j', 
+	'' => 'x', 
+	'' => 'q', 
+	'' => 'z', 
+}
+count = {}
+alphabet = "#ABCDEFGHIJKLMNOPQRSTUVWXYZ_"
+alphabet_index = {}
+alphabet.length.times do |idx|
+	alphabet_index[alphabet[idx, 1]] = idx
+end
+
+cryptotext = ""
+
+alphabet.each_char do |c|
+	count[c] = 0
+end
+
+rot = 0
+rot_offset = 1
+
+STDIN.each_char do |c|
+	if c == "\n"
+		next
+	end
+	cryptotext += alphabet[(alphabet_index[c] + rot) % alphabet.length, 1]
+	rot += rot_offset
+end
+
+cryptotext.each_char do |c|
+	count[c] += 1
+end
+
+cryptotext.each_char do |c|
+	if translation[c].nil?
+		print c
+		next
+	end
+	print translation[c]
+end
+puts
+
+count.each_key do |key|
+	STDERR.printf "%.3f%% (%d) %s", (100*count[key].to_f / cryptotext.length), count[key], key
+	if translation[key].nil?
+		STDERR.puts " *"
+	else
+		STDERR.puts " => " + translation[key]
+	end
+end
+
