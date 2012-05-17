781d1405a64c94972b372e1e5dcc751589e0b719
Peter Boström <pbos@kth.se>
2012-05-17 18:12:15 +0200

    some ecp progress

--- krypto12/hw3/ellipticcurvepoints/ellipticcurvepoints.py
+++ krypto12/hw3/ellipticcurvepoints/ellipticcurvepoints.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
+import sys
+
+def singular(p, a, b):
+	# discriminant for y^2 = x^3+ax+b is given
+	# by: -4a^3-27b^2
+	discriminant = (-4*a**3-27*b**2) % p
+
+	if discriminant == 0:
+		return 1 # singular
+	else:
+		return 0 # smooth
 
 def ecp(p, a, b):
-	return (-1, -1)
+	return (singular(p,a,b,), -1)
 
 # just i/o
 for line in sys.stdin:
